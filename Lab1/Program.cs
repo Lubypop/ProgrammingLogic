16119b66c0d32bc97f73deac3b25460d9305ba65
Lubypop <lpierce101@outlook.com>
2025-03-21 14:50:33 +0000

    Created Lab1

---
+++ Lab1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcone to IT-1050!");
+    }
+}

39671262d6b1f930304bd636a2850b3b7f8e580d
Lubypop <lpierce101@outlook.com>
2025-03-21 15:43:50 +0000

    Lab folders and current lab1 progress

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,11 +1,24 @@
 ﻿namespace Lab1;
 
-class Program
-{
+using System;
+
+  class Program
+  {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Welcone to IT-1050!");
-        
+        // Step 1
+        // Print your name and course title using Console.WriteLine()
+        Console.WriteLine("Lauren Pierce, Course IT-1050");
+        // Step 2
+        // Add a multi-line comment indicating Name: [Your Name] and Title: [IT-1050 - Lab 1]
+        Console.WriteLine("Name: Lauren Pierce");
+        Console.WriteLine("Course Title: IT-1050");
+        // Step 3
+        // int- Your favorite number.
+        // string- Your favorite programming language.
+        // double- The number of programs you've written before this.
+        // bool- Yes or No if have programming experience.
+
+
     }
-}
+  }
--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16119b66c0d32bc97f73deac3b25460d9305ba65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab7fda76d499710fb680af0536aec4d66c57ec2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

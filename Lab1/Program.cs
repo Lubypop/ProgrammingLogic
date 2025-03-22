f2bef9d8c3571bb25998e1348ec5fe635ca4bfee
Lubypop <lpierce101@outlook.com>
2025-03-22 13:23:28 +0000

    Removed commented code. Final

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,45 +1,4 @@
-﻿// namespace Lab1;
-
-// using System;
-// using System.Globalization;
-// using System.Net.Security;
-// using Microsoft.VisualBasic;
-
-// class Program
-//   {
-//     static void Main(string[] args)
-//     {
-//         // Step 1
-//         // Print your name and course title using Console.WriteLine()
-//         Console.WriteLine("Lauren Pierce, Course IT-1050");
-//         // Step 2
-//         // Add a multi-line comment indicating Name: [Your Name] and Title: [IT-1050 - Lab 1]
-//         Console.WriteLine("Name: Lauren Pierce");
-//         Console.WriteLine("Course Title: IT-1050");
-//         // Step 3
-//         // int- Your favorite number.
-//         int myFavoriteNumber = 9;
-//         string myStringName = "My favorite number is ";
-//         Console.WriteLine(myStringName + myFavoriteNumber);
-//         // string- Your favorite programming language.
-//         string myFavoriteLanguage= "My favorite programming language is C#";
-//         Console.WriteLine(myFavoriteLanguage);
-//         // double- The number of programs you've written before this.
-//         Console.WriteLine("I've written 0.0 programs before this");
-//         // bool- Yes or No if have programming experience.
-//         bool hasProgrammingExperience=false;
-//         Console.WriteLine(hasProgrammingExperience);
-//         const string mySchool = "Cuyahoga Community College";
-//         Console.WriteLine(mySchool);
-
-//     }
-//   }
-
-// ^ You will get rid of this after you are comfortable.
-
-
-
-namespace Lab1;
+﻿namespace Lab1;
 
 
 
@@ -58,7 +17,7 @@ class Program
         int myFavoriteNumber = 9;
         string myStringName = "My favorite number is ";
         string myFavoriteLanguage = "My favorite programming language is C#";
-        double timeProgramming = 1.0;
+        double timeProgramming = 0.0;
         bool hasProgrammingExperience = false;
 
         // Step 5a: Declare a constant of school name
@@ -66,41 +25,28 @@ class Program
 
         // Step 1:
         // Print your name and course title using Console.WriteLine()
-        Console.WriteLine("Lauren Pierce, Course IT-1050");
+        Console.WriteLine("Name: Lauren Pierce");
+        Console.WriteLine("Title: IT-1050");
 
         // Step 2:
         // Add a multi-line comment indicating Name: [Your Name] and Title: [IT-1050 - Lab 1]
-
-        
-        Console.WriteLine("Name: Lauren Pierce");
-        Console.WriteLine("Title: IT-1050");
-        
-
+        /*This is a multi-line comment. Name: Lauren Pierce 
+        Course title: IT-1050*/
+       
         // Step 3b: Print varialbles
         Console.WriteLine(myStringName + myFavoriteNumber);
         Console.WriteLine(myFavoriteLanguage);
-        Console.WriteLine("I have spent " + timeProgramming + " hours programming today");
+        Console.WriteLine("I have programmed 0 times before this");
         Console.WriteLine("Has Programming experience: " + hasProgrammingExperience);
 
         // Step 5b: Display constant of school name 
         Console.WriteLine("I am a student at " + MY_SCHOOL);
         
-
-
         // Step 4: (null)
 
         // Step 5: Use Constants (above for best practice)
 
         // Step 6: Use Meaningful Identifiers (verified)
-
-
-        // example of multiline comment
-        
-        /* 
-        Console.WriteLine(myStringName + myFavoriteNumber);
-        Console.WriteLine(myFavoriteLanguage); 
-        */
-
 
     }
   }
--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd7a6af06cdd65a1af9bf60cfcec9f5057054f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19d7f231044065dacead1bc91085debd9d90f78d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

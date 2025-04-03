3c051419c22d6e0202ad951fa3fc2a123a9fc636
Lubypop <lpierce101@outlook.com>
2025-04-03 21:24:55 +0000

    Working on Lab 3 Variable and If else statements

--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2597a45ae3270879ff61206063b970d2bab8436c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd540507346e15bdc006caeef11a93d9339092c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -4,6 +4,80 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Declare three integer variables: a = 10, b = 5, and c = 15.
+            int A = 10;
+            int B = 5;
+            int C = 15;  
+    
+        //Use comparison and logical operators to:
+        //Check if a is greater than b
+            Console.WriteLine(A > B);
+
+        //Check if a is less than c
+            Console.WriteLine(A < C );
+
+        //Check if a is greater than both b and c
+            Console.WriteLine( A > B && A > C);
+
+        //Combine expressions using && and ||
+            Console.WriteLine(A > B || A >C );
+
+ //Part 2: Boolean Logic
+        //Declare a bool variable called isRaining and set it to true.
+            bool isRaining = true;
+
+        // Declare another bool variable haveUmbrella and set it to false.
+            bool haveUmbrella = false;
+
+        //Write an if statement to print:
+        //"Take an umbrella!" if it’s raining and you don’t have one.
+        // "You're good to go!" otherwise.
+
+            if (isRaining) 
+          { 
+            Console.WriteLine("Take an Umbrella!");
+          }
+          else
+          {
+         
+            Console.WriteLine("You're good to go!");
+          }
+        {
+//Part 3: Conditional Logic – Movie Ticket Pricing
+ //Write a program that asks the user to enter their age. Based on their age, display the ticket price category:
+//If the age is less than 5, display: "Ticket is free!"
+If the age is between 5 and 12, display: "Child ticket: $5"
+If the age is between 13 and 64, display: "Standard ticket: $10"
+If the age is 65 or older, display: "Senior ticket: $6"
+Use int.Parse(Console.ReadLine()) (or Convert.ToInt32()) to read and parse the user’s input.
+       
+    
+
+
+
+//Part 4: Using a switch Statement
+  //Write a program that asks the user to enter a day of the week (1–7). Use a switch statement to display the name of the corresponding day:
+
+//1 - Monday
+//2 - Tuesday
+//3 - Wednesday
+//4 - Thursday
+//5 - Friday
+//6 - Saturday
+//7 - Sunday
+
+//If the user enters an invalid number, display: “Invalid day!”
+      
+
+
+      
+
+
+
+
+
+
+
+
     }
 }

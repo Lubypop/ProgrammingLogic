1b207a4637b6c81b8679d1e0ce1040aca79d0fec
Lubypop <lpierce101@outlook.com>
2025-05-05 22:22:57 -0400

    Update Animal.cs

--- Lab8/base/Animal.cs
+++ Lab8/base/Animal.cs
@@ -4,7 +4,6 @@ namespace Lab8
 {
   class Animal
   {
-    // public string name = "Animal";    < --- may use this but not needed
     public virtual void animalSound()
     {
       Console.WriteLine("The animal is making sound...");
